64799741e1b7ec5f8e6a2558bdd09249e91729f8
YuviPanda <yuvipanda@gmail.com>
2012-02-03 17:13:47 +0530

    Initial conversion to 1.4.x. Needs iOS work

--- src/org/wikipedia/WikipediaActivity.java
+++ src/org/wikipedia/WikipediaActivity.java
@@ -12,35 +12,11 @@ import com.phonegap.DroidGap;
 public class WikipediaActivity extends DroidGap {
     /** Called when the activity is first created. */
 	
-	public class WikipediaWebViewClient extends GapViewClient {
-		public WikipediaWebViewClient(DroidGap ctx) {
-			super(ctx);
-		}
-		
-		//@Override
-		//public void onLoadResource(WebView view, String url) {
-		//	Log.d("WikipediaWebViewClient", "OnLoadResource "+url);
-		//}
-		
-	}
-	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
-        // NearBy shit
-		SharedPreferences preferences = getSharedPreferences("nearby", MODE_PRIVATE);
-		SharedPreferences.Editor editor = preferences.edit();
-		editor.remove("doSearchNearBy");
-		editor.commit();
-		
-        super.loadUrl("file:///android_asset/www/index.html");
-        this.webViewClient = new WikipediaWebViewClient(this);
-        this.appView.setWebViewClient(this.webViewClient);
+		super.loadUrl("file:///android_asset/www/index.html");
     }
-    
-    @Override
-    public void onReceivedError(final int errorCode, final String description, final String failingUrl) {
-    	// no-op!
-    }
+
 }
